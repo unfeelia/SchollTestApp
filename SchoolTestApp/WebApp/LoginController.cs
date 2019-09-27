25ec2f00951dc9b7726894d3cfde11a352758a50
SukhorukovD <Salech198644@gmail.com>
2019-09-27 21:25:11 +0300

    TODO 1
    Auth cookie via ClaimsPrinciple: externalID + Roles

--- SchoolTestApp/WebApp/LoginController.cs
+++ SchoolTestApp/WebApp/LoginController.cs
@@ -1,4 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApp
@@ -20,6 +25,16 @@ namespace WebApp
             if (account != null)
             {
                 //TODO 1: Generate auth cookie for user 'userName' with external id
+
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, account.ExternalId),
+                    new Claim("Role", account.Role)
+                };
+
+                ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, "Role");
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
             }
             //TODO 2: return 404 if user not found
         }
